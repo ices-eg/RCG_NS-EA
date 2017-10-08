e446d5f28269cf4205cb1c86e6d55fe5823075c1
nmprista <31890653+nmprista@users.noreply.github.com>
2017-10-08 13:32:08 +0200

    Update func_heatmap_ices_rect_one_var.r
    replaced former "a" with "map" and included argument map_file in argument to facilitate load map from specific directory

--- RDBcheck/func_heatmap_ices_rect_one_var.r
+++ RDBcheck/func_heatmap_ices_rect_one_var.r
@@ -1,4 +1,4 @@
-heatmap_ices_rect_one_var<-function(x, Var, F, map.title, legend.title, nbreaks, xlim = c(-4.5,+14.5), ylim = c(49.75,62.25), legend.amp=1){
+heatmap_ices_rect_one_var<-function(x, Var, F, map.title, legend.title, nbreaks, xlim = c(-4.5,+14.5), ylim = c(49.75,62.25), legend.amp=1, map_file =""){
 # draws grid map with heat-mapped ices rectangles
 # x is a subset of data (with ICES_RECT= ices rectangles). 
 # F is the function that is applied to on each ices rectangle (e.g., sum, mean)
@@ -8,10 +8,12 @@ heatmap_ices_rect_one_var<-function(x, Var, F, map.title, legend.title, nbreaks,
 # Nuno Prista @ RCM NS&EA 2017
 
 # Example
-	#heatmap_ices_rect_one_var(x=data1$ceSub, Var = "KWDays", F = sum, map.title = "sum KWDays", xlim=c(-4.5,+14.5), ylim=c(49.75,62.25), legend.title="sum KWDays", nbreaks=6)
+	#heatmap_ices_rect_one_var(x=data1$ceSub, Var = "KWDays", F = sum, map.title = "sum KWDays", xlim=c(-4.5,+14.5), ylim=c(49.75,62.25), legend.title="sum KWDays", nbreaks=6, map_file ="001_Inputs/GSHHS_l_L1.shp"))
 
 require(rgdal)
-require(mapplots)	
+require(mapplots)
+
+map<-readOGR(map_file)	
 	
 #builds dataset per ICES_RECT
 ls1<-split(x, factor(x$StatisticalRectangle))
@@ -30,7 +32,7 @@ print(breaks)
 basemap(xlim=xlim, ylim=ylim, main = map.title)
 draw.rect()
 draw.grid(grd,breaks)
-par(new=T);plot(a, xlim=xlim, ylim=ylim, bg=FALSE, col="gray")
+par(new=T);plot(map, xlim=xlim, ylim=ylim, bg=FALSE, col="gray")
 legend.grid("topright", breaks=breaks*legend.amp, type=2, inset=0.02, title=legend.title)
 
 
